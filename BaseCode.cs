940ad4b7f247782d163aa27cce23ae307b67d26f
RockyTheProtogen <124413320+RockyTheProtogen@users.noreply.github.com>
2023-09-25 21:40:59 -0400

    Create BaseCode.cs and add code.
    Base Code to use in Turbowarp project.

---
+++ BaseCode.cs
@@ -0,0 +1,16 @@
+using Melonloader;
+using ReMod.Core;
+using System;
+using System.Reflection;
+
+[assembly: MelonInfo(typeof(SCRD.CLASS1), "Stuff", "1.0.0", "Stuff", downloadLink: "")]
+[assembly: MelonGame("vrchat", "oculus.")]
+[assembly: MelonColor(ConsoleColor.DarkMagenta)]
+
+namespace SCRD
+{
+  public class CLASS1 : MelonMod
+  {
+    //blank
+  }
+}
